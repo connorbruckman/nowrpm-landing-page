52e8050e0d4e2db17733945c3355b3d2f38af8b5
connorbruckman <111317506+connorbruckman@users.noreply.github.com>
2022-09-21 16:14:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-<script>
-  function onSubmit(token) {
-    document.getElementById("demo-form").submit();
-  }
-</script>
+$(document).ready(function() {
+    $(document).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var topDist = $("#container").position();
+        if (scroll > topDist.top) {
+            $('nav').css({"position":"fixed","top":"0"});
+        } else {
+            $('nav').css({"position":"static","top":"auto"});
+        }
+    });
+});
